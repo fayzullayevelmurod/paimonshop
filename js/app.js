1c19f0f2b3f23deecf8204946a3d164d6fa6186a
fayzullayevelmurod <elmurodfayzullayev77@gmail.com>
2023-11-09 11:23:14 +0500

    the end

--- js/app.js
+++ js/app.js
@@ -1,3 +1,17 @@
+const category_btn = document.querySelector('.category_btn');
+const region_btn = document.querySelector('.region_btn');
+const category_list_one = document.querySelector('.category_list_one');
+const category_list = document.querySelector('.category_list');
+
+category_btn.addEventListener('click', () => {
+    category_list_one.classList.toggle('active')
+});
+region_btn.addEventListener('click', () => {
+    category_list.classList.toggle('active')
+});
+
+
+
 const burger = document.querySelector('.header_burger');
 const nav_media = document.querySelector('.nav_media');
 const nav_media_close = document.querySelector('.nav_close');
@@ -9,4 +23,6 @@ burger.addEventListener('click', () => {
 
 nav_media_close.addEventListener('click', () => {
     nav_media.classList.remove('active')
-})
+});
+
+
